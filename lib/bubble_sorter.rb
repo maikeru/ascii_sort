3116129db7cd0beaf313b254186d82bfcc6dc282
P. Michael Holland <maikeru.horando@gmail.com>
2013-08-17 15:21:12 +0900

    Implemented bubble sort and a temporary runner to give a pretty output

--- lib/bubble_sorter.rb
+++ lib/bubble_sorter.rb
@@ -1,10 +1,24 @@
 class BubbleSorter
   def initialize(listener, input)
     @listener = listener
-    @input = input
+    @working_list = input
+    @unsorted_max_index = input.length - 2
   end
 
   def sort
-    @listener.step(@input)
+    while @unsorted_max_index >= 0 do
+      for index in (0 .. @unsorted_max_index)
+        left = @working_list[index]
+        right = @working_list[index + 1]
+        if (left > right)
+          @working_list[index] = right
+          @working_list[index + 1] = left
+        end
+        @listener.step(@working_list)
+      end
+      @unsorted_max_index -= 1
+    end
+    @working_list
   end
+
 end
--- spec/unit/bubble_sorter_spec.rb
+++ spec/unit/bubble_sorter_spec.rb
@@ -1,32 +1,35 @@
 require 'bubble_sorter'
 
-# Sorter needs
-  # Starting list
-  # Current result
-#
-  # Should the sorter return a value after each step (in which case we need to keep calling it to step)
-  # Or should it call out to a listener after each step?
-# Asciifier needs
-  # list of items at current sorting step
-
 describe BubbleSorter, "#sort" do
   context "with a sorted list" do
     input = (1 .. 10).to_a
     it "calls the listener after the first step" do
-      listener = double("listener")
-      expect(listener).to receive(:step).with(input)
+      listener = MockListener.new
       sorter = BubbleSorter.new(listener, input)
       sorter.sort
-    end
-
-    it "finishes after one pass (nine steps)" do
+      listener.last_event.should eq (1 .. 10).to_a
     end
   end
 
   context "reverse-sorted list" do
+    reverse_list = (1 .. 10).to_a.reverse
+    it "calls the listener with the sorted list" do
+      result_list = ()
+      listener = MockListener.new
+      sorter = BubbleSorter.new(listener, reverse_list)
+      sorter.sort.should eq (1 .. 10).to_a
+      #listener.last_event.should eq (1 .. 10).to_a
+    end
   end
 
   context "un-sorted list" do
+  end
+end
+
+class MockListener
+  attr_reader :last_event
+  def step input
+    @last_event = input
   end
 end
 
