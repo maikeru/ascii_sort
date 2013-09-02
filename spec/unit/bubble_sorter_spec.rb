c68eec4ca7aa122231aa631bf4fb83b9c2af5c71
P. Michael Holland <maikeru.horando@gmail.com>
2013-09-02 22:02:21 +0900

    focus tests on correct sorting

--- spec/unit/bubble_sorter_spec.rb
+++ spec/unit/bubble_sorter_spec.rb
@@ -1,28 +1,29 @@
 require 'bubble_sorter'
 
 describe BubbleSorter, "#sort" do
+  let(:listener) { MockListener.new }
   context "with a sorted list" do
-    input = (1 .. 10).to_a
-    it "calls the listener after the first step" do
-      listener = MockListener.new
-      sorter = BubbleSorter.new(listener, input)
-      sorter.sort
-      listener.last_event.should eq (1 .. 10).to_a
+    presorted_list = (1 .. 10).to_a
+    it "sorts a pre-sorted list" do
+      sorter = BubbleSorter.new(listener, presorted_list)
+      sorter.sort.should eq (1..10).to_a
     end
   end
 
   context "reverse-sorted list" do
     reverse_list = (1 .. 10).to_a.reverse
-    it "calls the listener with the sorted list" do
-      result_list = ()
-      listener = MockListener.new
+    it "sorts a reverse-sorted list" do
       sorter = BubbleSorter.new(listener, reverse_list)
       sorter.sort.should eq (1 .. 10).to_a
-      #listener.last_event.should eq (1 .. 10).to_a
     end
   end
 
   context "un-sorted list" do
+    random_list = [7, 4, 1, 2, 10, 7, 8, 10, 1, 4 ]
+    it "sorts a random list" do
+      sorter = BubbleSorter.new(listener, random_list)
+      sorter.sort.should eq [1, 1, 2, 4, 4, 7, 7, 8, 10, 10]
+    end
   end
 end
 
